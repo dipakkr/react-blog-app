fe2ff937c150ef338de7c258d4c4a8d55790a387
Gerald Addo-Tetteh <geraldadt@outlook.com>
2021-04-15 17:14:02 +0000

    added screenshots & updated README.md && changed styles of add-comment form && provided error checking for retrieving articles

--- src/components/Comment/AddCommentForm.js
+++ src/components/Comment/AddCommentForm.js
@@ -1,37 +1,48 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const AddCommentForm = ({ articleName, setArticleInfo }) => {
-    const [username, setUsername] = useState('');
-    const [commentText, setCommentText] = useState('');
+  const [username, setUsername] = useState("");
+  const [commentText, setCommentText] = useState("");
 
-    const addComment = async () => {
-        const result = await fetch(`/api/articles/${articleName}/add-comment`, {
-            method: 'post',
-            body: JSON.stringify({ username, text: commentText }),
-            headers: {
-                'Content-Type': 'application/json',
-            }
-        });
-        const body = await result.json();
-        setArticleInfo(body);
-        setUsername('');
-        setCommentText('');
-    }
+  const addComment = async () => {
+    const result = await fetch(`/api/articles/${articleName}/add-comment`, {
+      method: "post",
+      body: JSON.stringify({ username, text: commentText }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const body = await result.json();
+    setArticleInfo(body);
+    setUsername("");
+    setCommentText("");
+  };
 
-    return (
-        <div id="add-comment-form">
-            <h3>Add a Comment</h3>
-            <label>
-                Name:
-                <input type="text" value={username} onChange={(event) => setUsername(event.target.value)} />
-            </label>
-            <label>
-                Comment:
-                <textarea rows="4" cols="50" value={commentText} onChange={(event) => setCommentText(event.target.value)} />
-            </label>
-            <button onClick={() => addComment()}>Add Comment</button>
-        </div>
-    );
-}
+  return (
+    <div id="add-comment-form">
+      <h3>Add a Comment</h3>
+      <div className="add-comment-form--input-fields">
+        <label>
+          Name:
+          <input
+            type="text"
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+          />
+        </label>
+        <label>
+          Comment:
+          <textarea
+            rows="4"
+            cols="50"
+            value={commentText}
+            onChange={(event) => setCommentText(event.target.value)}
+          />
+        </label>
+      </div>
+      <button onClick={() => addComment()}>Add Comment</button>
+    </div>
+  );
+};
 
 export default AddCommentForm;
